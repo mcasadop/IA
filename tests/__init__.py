d973fc4b588e8e1af56e140cdcf034c6a1a3a38c
mcasadop <124916747+mcasadop@users.noreply.github.com>
2023-03-08 22:34:27 +0100

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,17 @@
-print('I have tried to create a unittest file, but in my script I have no functions (def), so I don't know how to do it')
+import unittest
+import os
+import requests
+
+class TestIA(unittest.TestCase):
+    
+    def test_resources_folder_exists(self):
+        folder_path = '/IA/ia/resources'
+        self.assertTrue(os.path.exists(folder_path))
+        
+    def test_grobid_is_running(self):
+        url = 'http://grobid:8070/api/isalive'
+        response = requests.get(url)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
